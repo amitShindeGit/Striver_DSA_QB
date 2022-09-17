ecff4ac406e6289d3123d2ff9a08041ec4ff293c
amitShindeGit <99amitshinde@gmail.com>
2022-09-18 02:17:02 +0530

    Flattening a Linked List sln

---
+++ Linked List-ll/Flattening a Linked List.cpp
@@ -0,0 +1,39 @@
+//GFG 
+Node *mergeLL(NOde *a, Node *b){
+       Node* temp = new Node(0);
+       Node* res = temp;
+       
+       while(a != NULL && b != NULL){
+           if(a->data > b->data){
+               temp->bottom = b;
+               temp = temp->bottom;
+               b = b->bottom;
+           }else{
+               temp->bottom = a;
+               temp = temp->bottom;
+               a = a->bottom;
+           }
+       }
+       
+       if(a) temp->bottom = a;
+       else temp->bottom = b;
+   }
+
+       
+       return res->bottom;
+}
+   
+Node *flatten(Node *root)
+{
+   // Your code here
+   if(root == NULL || root->next == NULL){
+       return root;
+   }
+   
+   root->next = flatten(root->next);
+   
+   root = mergeLL(root, root->next);
+   
+   return root;
+  
+}
